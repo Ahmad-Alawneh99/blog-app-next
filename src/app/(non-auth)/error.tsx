aa9f2bfb0553c97a315caeeb1c09b7f9c7b72b9f
Ahmad Alawneh <aalawneh19@gmail.com>
2024-12-24 16:10:30 +0300

    catch-all error page

---
+++ src/app/(non-auth)/error.tsx
@@ -0,0 +1,13 @@
+import Error from '../../components/Error/Error';
+
+export default function ErrorPage({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return (
+    <main>
+      <Error title="Oh no! Error" message={`Something went wrong on the server: ${error.message}`} />
+    </main>
+  );
+}
