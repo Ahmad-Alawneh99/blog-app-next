9009eca0a8db773eea69c786dce662c34b7338c6
Ahmad Alawneh <aalawneh19@gmail.com>
2024-12-24 16:17:16 +0300

    update error messages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,14 +1,10 @@
 'use client';
 import Error from '../components/Error/Error';
 
-export default function ErrorPage({
-  error,
-}: {
-  error: Error & { digest?: string };
-}) {
+export default function ErrorPage() {
   return (
     <main>
-      <Error title="Oh no! Error" message={`Something went wrong on the server: ${error.message}`} />
+      <Error title="Oh no! Error!" message="Something went wrong on the server, please try again later" />
     </main>
   );
 }
